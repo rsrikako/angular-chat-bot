f4e5e5068405f59ec1995dac17298061159803f4
rsrikako <ravi.kkd.1995@gmail.com>
2020-11-11 00:11:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chat-bot-e7brxr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-chat-bot-e7brxr)
---
+++ src/app/angular-bot/chat/chat.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Message, ChatService } from "../chat.service";
+@Component({
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"]
+})
+export class ChatComponent implements OnInit {
+  messages: Message[] = [];
+  value: string;
+
+  constructor(public chatService: ChatService) {}
+
+  ngOnInit() {
+    this.chatService.conversation.subscribe(val => {
+      this.messages = this.messages.concat(val);
+      setTimeout(() => {
+        this.updateScroll(), 500;
+      });
+    });
+  }
+
+  sendMessage() {
+    this.chatService.getBotAnswer(this.value);
+    this.value = "";
+  }
+
+  updateScroll() {
+    document.getElementById(
+      "messages-area"
+    ).scrollTop = document.getElementById("messages-area").scrollHeight;
+  }
+}
